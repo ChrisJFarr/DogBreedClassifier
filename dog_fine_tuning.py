49f8d2e27b77eda538352ad036e4ee5dba0acd64
ChrisJFarr <chris.farr@cerner.com>
2018-03-12 21:33:49 -0500

    Trouble with reloading weights

--- dog_fine_tuning.py
+++ dog_fine_tuning.py
@@ -16,35 +16,20 @@ import pandas as pd
 # from keras.utils.generic_utils import CustomObjectScope
 
 
-if __name__ == "__main__":
+# Training steps:
+# Start with pre-trained MobileNet
+# Freeze all of the network and train the dense layers using adam
+# Recompile the model and unfreeze the entire Mobile Net
+# Load the best weights from the first training session
+# Train the entire model using adam with a low learning rate
 
-    x_train = np.load("saved_data/x_train.npy")
-    y_train = np.load("saved_data/y_train.npy")
 
-    x_test = np.load("saved_data/x_test.npy")
-    y_test = np.load("saved_data/y_test.npy")
+def build_model(freeze_pretrained=True):
+    conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 
-    x_valid = np.load("saved_data/x_valid.npy")
-    y_valid = np.load("saved_data/y_valid.npy")
-
-    # Load in ResNet50 model with imagenet weights
-    # conv = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-    # conv = ResNet50(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
-    conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3),
-                             pooling='avg')
-
-    # with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,
-    #                         'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
-    #     model = load_model('weights.hdf5')
-    # conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-
-    # Freeze all but last x number of layers (4)
-    # for layer in conv.layers[:-4]:
-    #     layer.trainable = False
-
-    # Check the trainable status of the individual layers
-    for layer in conv.layers:
-        print(layer, layer.trainable)
+    if freeze_pretrained:
+        for layer in conv.layers:
+            layer.trainable = False
 
     # Create the model
     model = models.Sequential()
@@ -60,27 +45,37 @@ if __name__ == "__main__":
     model.add(layers.Dropout(0.4))
     model.add(layers.Dense(133, activation='softmax'))
 
+    # Show summary for verification
+    # Check the trainable status of the individual layers
+    for layer in conv.layers:
+        print(layer, layer.trainable)
+
     # Show a summary of the model. Check the number of trainable parameters
     model.summary()
 
+    return model
+
+
+if __name__ == "__main__":
+
+    x_train = np.load("saved_data/x_train.npy")
+    y_train = np.load("saved_data/y_train.npy")
+
+    x_test = np.load("saved_data/x_test.npy")
+    y_test = np.load("saved_data/y_test.npy")
+
+    x_valid = np.load("saved_data/x_valid.npy")
+    y_valid = np.load("saved_data/y_valid.npy")
+
+    # with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,
+    #                         'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
+    #     model = load_model('weights.hdf5')
+    # conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+
+    model = build_model(True)
     # Setup the data generators
-    train_datagen = ImageDataGenerator(
-        preprocessing_function=preprocess_input,
-        rotation_range=5,
-        width_shift_range=0.01,
-        height_shift_range=0.01,
-        zoom_range=0.01,
-        shear_range=0.01,
-        horizontal_flip=True,
-        vertical_flip=False
-    )
 
-    # train_datagen = ImageDataGenerator(
-    #     preprocessing_function=preprocess_input,
-    #     shear_range=0.2,
-    #     zoom_range=0.2,
-    #     horizontal_flip=True)
-
+    train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
@@ -96,14 +91,60 @@ if __name__ == "__main__":
     validation_generator = validation_datagen.flow(x_valid, y_valid, batch_size=val_batchsize)
 
     # Compile the model
-    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(1e-5), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(1e-4), metrics=['accuracy'])
 
-    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.incresv2.hdf5',
+    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.mobile_net_pre.hdf5',
                                    verbose=1, save_best_only=True)
 
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=100, verbose=1, mode='auto')
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1, mode='auto')
 
     # Train the model
+    history = model.fit_generator(
+        train_generator,
+        epochs=1,
+        validation_data=validation_generator,
+        steps_per_epoch=len(x_train) // train_batchsize,
+        validation_steps=len(x_valid) // val_batchsize,
+        callbacks=[checkpointer, early_stopping],
+        verbose=1)
+
+    df = pd.DataFrame(history.history)
+    # df.plot()
+    df.plot()
+
+    # Load weights and save the model
+    model = build_model(False)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(1e-5), metrics=['accuracy'])
+    model.load_weights('saved_models/weights.best.mobile_net_pre.hdf5')
+
+    for layer in conv.layers:
+        layer.trainable = True
+
+    # Test accuracy
+    # TODO START HERE! Load saved model
+    # model = models.load_model('incresv2.h5')
+    predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
+    test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(predictions)
+    print('Test pre-accuracy: %.4f%%' % test_accuracy)
+
+    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.mobile_net.hdf5',
+                                   verbose=1, save_best_only=True)
+
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=15, verbose=1, mode='auto')
+
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        rotation_range=20,
+        width_shift_range=0.20,
+        height_shift_range=0.20,
+        zoom_range=0.20,
+        shear_range=0.20,
+        horizontal_flip=True,
+        vertical_flip=False
+    )
+
+    train_generator = train_datagen.flow(x_train, y_train, batch_size=train_batchsize)
+
     history = model.fit_generator(
         train_generator,
         epochs=1000,
@@ -113,32 +154,12 @@ if __name__ == "__main__":
         callbacks=[checkpointer, early_stopping],
         verbose=1)
 
-    # Load weights and save the model
-    model.load_weights('saved_models/weights.best.incresv2.hdf5')
-    model.save('bestmobnet.h5')
-    # FInished 11 iterations
-
-    # Test accuracy
-    # TODO START HERE! Load saved model
-    # model = models.load_model('incresv2.h5')
-    predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
-    test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test , axis=1)) / len(predictions)
-    print('Test accuracy: %.4f%%' % test_accuracy)
-
-    # lr: 1e-4, 60+ val and test acc, changed shape to vgg orig and added drop
-    # lr: 1e-5, vgg shape, no drop out: 57% val
-    # lr: 1e-5, vgg shape, w/ 50/40 drop out: 68% test: model.save("vgg_68.h5")
-    # lr: 1e-5, vgg shape, w/ 30/10 drop out: 63% test
-    # lr: 5e-5, vgg shape, w/ 50/40 drop out: 66.7% test
-
-    # adamax default, incres 1776, 1776, 1492 .5/.5/.5/.4 drop out: 65.9% test
-    # sgd  lr: 1e-4, mom: .9, 875, 492 drop out: .5/.4:
-
-    # Add additional layers
-    # Add additional nodes
-    # Try new pretrained models
-    # plot visual
     df = pd.DataFrame(history.history)
     # df.plot()
     df.plot()
+
+    model.load_weights('saved_models/weights.best.mobile_net.hdf5')
+    predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
+    test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(predictions)
+    print('Test accuracy: %.4f%%' % test_accuracy)
 
