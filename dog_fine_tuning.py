2584e05f8e3b434850c8f9a3f079472ba2e331a7
ChrisJFarr <chris.farr@cerner.com>
2018-03-13 21:08:04 -0500

    MobileNet score 79% final training run
    Finalizing the remaining questions and exercises.

--- dog_fine_tuning.py
+++ dog_fine_tuning.py
@@ -4,16 +4,12 @@ import numpy as np
 from keras import models
 from keras import layers
 from keras.preprocessing.image import ImageDataGenerator
-from keras.applications import ResNet50, VGG16, MobileNet, InceptionResNetV2
-from keras.applications.inception_resnet_v2 import preprocess_input
+from keras.applications import MobileNet, mobilenet
+from keras.applications.mobilenet import preprocess_input
+from keras.models import load_model
 from keras import optimizers
-# from sklearn.preprocessing import MinMaxScaler
-# import random
-# from glob import glob
-# import sys
-from matplotlib import pyplot as plt
 import pandas as pd
-# from keras.utils.generic_utils import CustomObjectScope
+from matplotlib import pyplot as plt
 
 
 # Training steps:
@@ -22,38 +18,6 @@ import pandas as pd
 # Recompile the model and unfreeze the entire Mobile Net
 # Load the best weights from the first training session
 # Train the entire model using adam with a low learning rate
-
-
-def build_model(freeze_pretrained=True):
-    conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-
-    if freeze_pretrained:
-        for layer in conv.layers:
-            layer.trainable = False
-
-    # Create the model
-    model = models.Sequential()
-
-    # Add the vgg convolutional base model
-    model.add(conv)
-
-    # Add new layers
-    # model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1776, activation='relu'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1492, activation='relu'))
-    model.add(layers.Dropout(0.4))
-    model.add(layers.Dense(133, activation='softmax'))
-
-    # Show summary for verification
-    # Check the trainable status of the individual layers
-    for layer in conv.layers:
-        print(layer, layer.trainable)
-
-    # Show a summary of the model. Check the number of trainable parameters
-    model.summary()
-
-    return model
 
 
 if __name__ == "__main__":
@@ -67,20 +31,40 @@ if __name__ == "__main__":
     x_valid = np.load("saved_data/x_valid.npy")
     y_valid = np.load("saved_data/y_valid.npy")
 
-    # with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,
-    #                         'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
-    #     model = load_model('weights.hdf5')
-    # conv = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+    # Create the model
+    model = models.Sequential()
 
-    model = build_model(True)
+    # Add the vgg convolutional base model
+    model.add(MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg'))
+    model.add(layers.Dense(1776, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1492, activation='relu'))
+    model.add(layers.Dropout(0.4))
+    model.add(layers.Dense(133, activation='softmax'))
+
+    # Show summary for verification
+    # Check the trainable status of the individual layers
+    # Freeze layers for first training session
+    for layer in model.layers[0].layers:
+        layer.trainable = False
+
     # Setup the data generators
 
-    train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        rotation_range=5,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1,
+        shear_range=0.1,
+        horizontal_flip=True,
+        vertical_flip=True
+    )
     validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
     # Change the batchsize according to your system RAM
-    train_batchsize = 32
+    train_batchsize = 50
     val_batchsize = 10
     image_size = 224
     train_dir = 'dogImages/train'
@@ -91,7 +75,7 @@ if __name__ == "__main__":
     validation_generator = validation_datagen.flow(x_valid, y_valid, batch_size=val_batchsize)
 
     # Compile the model
-    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(1e-4), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adamax(), metrics=['accuracy'])
 
     checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.mobile_net_pre.hdf5',
                                    verbose=1, save_best_only=True)
@@ -101,24 +85,21 @@ if __name__ == "__main__":
     # Train the model
     history = model.fit_generator(
         train_generator,
-        epochs=1,
+        epochs=100,
         validation_data=validation_generator,
-        steps_per_epoch=len(x_train) // train_batchsize,
+        steps_per_epoch=(len(x_train) // train_batchsize) * 2,
         validation_steps=len(x_valid) // val_batchsize,
         callbacks=[checkpointer, early_stopping],
         verbose=1)
 
+    # Plot training history
     df = pd.DataFrame(history.history)
-    # df.plot()
     df.plot()
+    plt.show()
 
-    # Load weights and save the model
-    model = build_model(False)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(1e-5), metrics=['accuracy'])
+    # Load weights
     model.load_weights('saved_models/weights.best.mobile_net_pre.hdf5')
-
-    for layer in conv.layers:
-        layer.trainable = True
+    model.save("pre_mobilenet.h5")
 
     # Test accuracy
     # TODO START HERE! Load saved model
@@ -126,6 +107,11 @@ if __name__ == "__main__":
     predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
     test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(predictions)
     print('Test pre-accuracy: %.4f%%' % test_accuracy)
+
+    for layer in model.layers[0].layers[1:]:
+        layer.trainable = True
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adamax(1e-5), metrics=['accuracy'])
 
     checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.mobile_net.hdf5',
                                    verbose=1, save_best_only=True)
@@ -140,26 +126,37 @@ if __name__ == "__main__":
         zoom_range=0.20,
         shear_range=0.20,
         horizontal_flip=True,
-        vertical_flip=False
+        vertical_flip=True
     )
 
     train_generator = train_datagen.flow(x_train, y_train, batch_size=train_batchsize)
 
     history = model.fit_generator(
         train_generator,
-        epochs=1000,
+        epochs=500,
         validation_data=validation_generator,
         steps_per_epoch=(len(x_train) // train_batchsize) * 5,  # Why not 5x training data if aug.?
         validation_steps=len(x_valid) // val_batchsize,
         callbacks=[checkpointer, early_stopping],
         verbose=1)
 
+    # Plot training history
     df = pd.DataFrame(history.history)
-    # df.plot()
     df.plot()
+    plt.show()
 
     model.load_weights('saved_models/weights.best.mobile_net.hdf5')
     predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
     test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(predictions)
     print('Test accuracy: %.4f%%' % test_accuracy)
 
+    model.save("mobilenet.h5")
+
+    model = load_model('mobilenet.h5', custom_objects={
+        'relu6': mobilenet.relu6,
+        'DepthwiseConv2D': mobilenet.DepthwiseConv2D})
+
+    model.load_weights('saved_models/weights.best.mobile_net.hdf5')
+    predictions = model.predict_generator(test_datagen.flow(x_test, shuffle=False))
+    test_accuracy = 100 * np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(predictions)
+    print('Test accuracy: %.4f%%' % test_accuracy)
